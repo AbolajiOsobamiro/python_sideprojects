67f022f8671f7489272ef54e46dce953bddbe201
Daniel <danielfieldman222@gmail.com>
2024-09-01 01:11:11 +0100

    Pushing

---
+++ shapes.py
@@ -0,0 +1,44 @@
+number = int(input("Enter any number: "))
+
+
+#Right Angled Triangle with n number of rows
+#for n in range(number):
+ #   print("*" * (n+1))
+
+
+#Square with n number of rows
+#for n in range(number):
+ #   print("*" * (number))
+
+
+#Isoceles Triangle with n number of rows
+#for n in range(number):
+  #      print(" " * (number-n-1) + "*" * ((2*n)+1) + " " * (number-n-1))
+
+
+#Rhombus with n number of rows
+#for n in range(number):
+ #   print(" " * (number-n-1) + "*" * ((2*n)+1) + " " * (number-n-1))
+#for n in range(number):
+ #   print(" " * (n) + "*" * (2*(number-n)-1) + " " * (n) )
+
+
+#Parallelogram with n number of rows
+#for n in range(number):
+ #    print(" " * (number-n) + "*" * (number) )
+
+
+#Trapezoid with n number of rows, preferably with n greater than 7
+#for n in range(number):
+ #   if n >= 3:
+  #      print(" " * (number-n-1) + "*" * ((2*n)+1) + " " * (number-n-1))
+
+#Trapezium with n number of rows, preferably with n greater than 7
+#for n in range(number):
+    #if n >= 3:
+        #print("*" * ((2*n)+1) + " " * (number-n-1))
+
+
+#Circle with radius n
+for n in range(number):
+    print()
